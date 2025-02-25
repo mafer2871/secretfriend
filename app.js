e310859e330128ab8e81dbba10479231ca79feef
mafer2871 <mfrv04@gmail.com>
2025-02-25 18:47:16 -0500

    fusion de agregarAmigo y generateFriendName

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// definición de variables
 let friendsList = [];
+let sortedFriends = [];
 let secretFriend = '';
 let numbersList = [];
 let gameStarted = false;
-
+let currentFriendIndex = 0;
 
 
 function agregarAmigo() {
 
     // Impedir q se agreguen nombres a lista hasta no terminar este sorteo
     if (gameStarted){
-        alert('No se puede ingresar más nombres hasta terminar este sorteo');
+        //alert('No se puede ingresar más nombres hasta terminar este sorteo');
+        location.reload();
     } else {
         // Validar q el nombre no sea vacio o tenga números
         let validCharacters = /^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$/; // Solo letras y espacios
@@ -44,37 +46,32 @@ function sortearAmigo() {
         alert('Debe de haber al menos dos amigos para sortear');
     } else {
         gameStarted = true;
-        secretFriend = generateFriendName();
-        if (secretFriend) {
-            let chain = `Tu amigo secreto sorteado es ${secretFriend}`;
-            cleanAmigos();
-            resultado.innerHTML = "";
-            showFinalLists(resultado,chain);
-        }
+        let addButton = document.getElementById('add');
+        addButton.textContent = "Reiniciar";
+		// Verificar si ya se asignaron todos los amigos secretos
+		if (sortedFriends.length === friendsList.length) {
+            alert('Ya se sortearon todos los amigos registrados.  Para comenzar un nuevo juego, presiona Reiniciar');
+			return; 
+		}
+		 // Tomar el siguiente amigo que aún no tiene asignado un amigo secreto
+		currentFriendIndex = sortedFriends.length;
+		let generatedNumber = Math.floor(Math.random() * friendsList.length);
+
+		// Verificar que no se sortee a sí mismo ni que ya haya sido asignado
+		while (generatedNumber === currentFriendIndex || numbersList.includes(generatedNumber)) {
+			generatedNumber = Math.floor(Math.random() * friendsList.length);
+		}
+		// Guardar indices generados y amigos sorteados
+		numbersList.push(generatedNumber); 
+		sortedFriends.push(friendsList[generatedNumber]); 
+        let chain = `El amigo secreto de ${friendsList[currentFriendIndex]} es ${friendsList[generatedNumber]}`;
+        console.log(chain);
+        cleanAmigos();
+        resultado.innerHTML = "";
+        showFinalLists(resultado,chain);
+     
     }
 } //fin sortearAmigo
-
-function generateFriendName() {
-    let generatedNumber = Math.floor(Math.random()*friendsList.length);
-    
-    //Controlar si se han sorteado todos los amigos
-    if (numbersList.length == friendsList.length) {
-		alert('Ya se sortearon todos los amigos registrados. El juego se reiniciará automáticamente');
-        setTimeout(() => {
-            location.reload();
-        }, 1000);
-
-    } else {
-        // Validar para no repetir indices 
-        if (numbersList.includes(generatedNumber)) { 
-            return generateFriendName();
-        } else {
-            numbersList.push(generatedNumber);
-            console.log(numbersList);
-            return friendsList[generatedNumber];
-        }   
-    }
-} // fin generateFriendName
 
 function showFinalLists(lista, valor) {
     // Se usa para mostrar en pantalla la lista de amigos y el amigo secreto
